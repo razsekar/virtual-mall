c9fe9513b5243bbe4c60442bba2a211be687736e
RajaSekhar <razsekar@gmail.com>
2021-03-05 20:44:24 +0530

    Products API generation and MySQL connection

--- node-apis/api/models/Products.js
+++ node-apis/api/models/Products.js
@@ -8,7 +8,10 @@
 module.exports = {
 
   attributes: {
-
+    name: 'string',
+    quantity: 'number',
+    price: 'number',
+    image: 'string'
     //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
     //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
     //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
--- node-apis/config/datastores.js
+++ node-apis/config/datastores.js
@@ -48,8 +48,8 @@ module.exports.datastores = {
     *    (See https://sailsjs.com/config/datastores for help.)                 *
     *                                                                          *
     ***************************************************************************/
-    // adapter: 'sails-mysql',
-    // url: 'mysql://user:password@host:port/database',
+    adapter: 'sails-mysql',
+    url: 'mysql://vm_user:0uIceOSr3Efq217Z@fbnew.civmcqagdjyd.ap-south-1.rds.amazonaws.com:3306/virtual_mall',
 
   },
 
